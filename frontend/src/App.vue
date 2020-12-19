f6d8c4021442452fee3a8ec6b180769d9c0b96ff
Murat Erkin Çiçek <murerkinn@gmail.com>
2020-12-19 12:37:02 +0300

    Create basic app layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,13 @@
 <template lang="pug">
   #app
-    router-view
+    a-layout#components-layout-demo-top.layout(theme='light')
+      a-layout-content
+        router-view
 </template>
 
 <style lang="scss">
+.ant-layout {
+  min-height: 100vh;
+  background: #fefefe !important;
+}
 </style>
