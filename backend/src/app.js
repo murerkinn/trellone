e4abcf2ddf4d803d207a1ff52f94fd99958a1b12
Murat Erkin Çiçek <murerkinn@gmail.com>
2020-12-19 12:36:35 +0300

    Implement session and user router

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,15 @@ var createError = require('http-errors')
 var express = require('express')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const passport = require('passport')
 const cors = require('cors')
+
+const User = require('./models/user')
+const { mongoose } = require('./bootstrap')
+
+var userRouter = require('./routes/user')
 
 var app = express()
 app.use(
@@ -14,10 +22,32 @@ app.use(
 
 app.set('trust proxy', 1)
 
+app.use(
+  session({
+    store: new MongoStore({ mongooseConnection: mongoose.connection, stringify: false }),
+    secret: 'thisissupposedtobeasecret',
+    cookie: {
+      maxAge: 14 * 24 * 60 * 60 * 1000,
+      sameSite: process.env.NODE_ENV == 'production' && 'none',
+      secure: process.env.NODE_ENV == 'production',
+    },
+  })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(User.createStrategy())
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+
+app.use('/api/account', userRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
