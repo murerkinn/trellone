b2a30642e67dd64238cab1347f95a94dffa512c0
Murat Erkin Çiçek <murerkinn@gmail.com>
2020-12-19 12:37:19 +0300

    Create store module for account

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,18 +2,21 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
 
+import account from './account'
+
 Vue.use(Vuex)
 
 axios.defaults.baseURL = process.env.VUE_APP_BASE_PATH
 axios.defaults.withCredentials = true
 
 const store = new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  modules: {
+    account
+  }
 })
 
 export default async function init() {
+  await store.dispatch('account/init')
+
   return store
 }
