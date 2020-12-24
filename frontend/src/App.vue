2feece4086e6b66b177c01097025f73f1b1ea57d
Murat Erkin Çiçek <murerkinn@gmail.com>
2020-12-24 22:06:07 +0300

    Update App to display dynamic layouts

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,37 @@
 <template lang="pug">
   #app
-    a-layout#components-layout-demo-top.layout(theme='light')
-      a-layout-content
-        router-view
+    navbar(@do-logout="doLogout")
+    a-layout.layout(theme='light')
+      component(:is="layout" :selectedKeys="selectedKeys")
+
 </template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+import Navbar from './components/Navbar'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  },
+  computed: {
+    selectedKeys() {
+      return [this.$route.name]
+    },
+    layout() {
+      return `${this.$route.meta.layout || 'default'}-layout`
+    }
+  },
+  methods: {
+    ...mapActions('account', ['logout']),
+    async doLogout() {
+      await this.logout()
+      this.$router.push('/login?logoutSuccess=1')
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 .ant-layout {
